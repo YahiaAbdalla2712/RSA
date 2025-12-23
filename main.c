5cbb49e352ffcc2217cd638583a2b06ab6b79299
YahiaAbdalla2712 <yahiaabdalla2712@gmail.com>
2025-12-23 18:29:04 +0200

    add montgomery reduce and multiplication

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ typedef unsigned int uint32;
 typedef long long int64;
 typedef unsigned long long uint64;
 
+#define R 0x100000000ULL
+
 uint32 extended_ecluid(uint32 m ,uint32 b){
     int64 a1 = 1, a2 = 0, a3 = m;
     int64 b1 = 0, b2 = 1, b3 = b;
@@ -35,6 +37,18 @@ uint32 extended_ecluid(uint32 m ,uint32 b){
         b2 = t2;
         b3 = t3;
     }
+}
+
+uint32 montgomery_reduce(uint64 x,uint32 n){
+    uint64 t1 = x;
+    uint64 t2 = (uint64)((uint32)x * extended_ecluid(R,n))*n;
+    uint32 r = (uint32)((t1 - t2) >> 32);
+    r += n & -(t1 < t2);
+    return r;
+}
+
+uint32 montgomery_mul(uint32 a, uint32 b) {
+    return mont_reduce((uint64)a * b);
 }
 
 
